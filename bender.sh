2f3bd8e9b13d7a90ac493e367940f97765265b68
Rodolfo Ovalles <rovalles0100@gmail.com>
2021-03-07 23:18:49 -0600

    changing it up

--- bender.sh
+++ bender.sh
@@ -1,47 +1,25 @@
 #!/bin/bash
 
-function benderAutoload(){
-  for file in ~/dotfiles/bundle/*.sh; do
-      [ -r "$file" ] && source "$file"
-  done
-  unset file
+function load(){
+    for i in "${LoadScripts[@]}"
+    do
+        local f="$HOME/dotfiles/bundle/load/$i"
+        if [ -f "$f" ] ; then
+            . "$f";
+        fi
+    done
 }
 
-function benderInstall(){
-  local file="$1"
-  local link=~/.$(basename  "$file")
-
-  if [ ! -e "$link" ]; then
-      ln -sv "$file" "$link"
-  else
-    echo "$file already linked"
-  fi
+function link(){
+    for x in "${LinkScripts[@]}"
+    do
+        local hf="$HOME/.$x"
+        local df="$HOME/dotfiles/bundle/symlink/$x"
+        if [ ! -e "$hf" ] ; then
+            ln -s "$df" "$hf"
+        fi
+    done
 }
 
-
-function benderRemove(){
-  local file="$1"
-
-  base=$(basename  "$file")
-  rm ~/."$base"
-  unlink ~/."$base"
-  echo "~.$base unlinked"
-}
-
-
-function bender(){
-  local cmd="$1"
-
-  for file in $dotfiles/bundle/*/symlinks/*
-  do
-    if [[ "$cmd" == "remove" ]] ; then
-        benderRemove "$file"
-    elif [[ "$cmd" == "install" ]] ; then
-        benderInstall "$file"
-    else
-      echo "No idea what command that is."
-    fi
-  done
-}
-
-benderAutoload
+load
+link
